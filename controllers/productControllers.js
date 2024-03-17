f521389cf438a7d93236c4af17bc44ad870a3ff1
Aremu Olakunle Umaru <colakunleumaru@gmail.com>
2024-03-17 20:26:40 +0100

    added the product functionality

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -34,3 +34,69 @@ export const createProduct=async(req,res,next)=>{
         next(err)       
     }
 }
+
+
+//@desc    Get all product
+//@route   GET /api/v1/product
+//@access  Public
+
+
+export const getAllProducts=async(req,res,next)=>{
+    try {
+        let productsData=productModel.find()
+
+        //search product with name
+        if(req.query.name){
+            productsData.find({name:{$regex:req.query.name, $options:"i"}})
+        }
+
+        //search product with brand
+        if(req.query.brand){
+            productsData.find({brand:{$regex:req.query.brand, $options:"i"}})
+        }
+
+        //search product with sizes
+        if(req.query.sizes){
+            productsData.find({sizes:{$regex:req.query.sizes, $options:"i"}})
+        }
+
+        //search product with colors
+        if(req.query.colors){
+            productsData.find({colors:{$regex:req.query.colors, $options:"i"}})
+        }
+
+        //search product with category
+        if(req.query.category){
+            productsData.find({category:{$regex:req.query.category, $options:"i"}})
+        }
+
+        //search product with price range
+        if(req.query.priceRange){
+            const priceRange=req.query.price.split("-")
+            productsData.find({price:{$gte:priceRange[0], $lte:priceRange[1]}})
+        }
+        //pagination
+        // const page=parseInt(req.query.page)?parseInt(req.query.page): 1
+        // const limit=parseInt(req.query.limit)?parseInt(req.query.limit): 10
+        // const startIndex=(page-1)*limit
+        // const endIndex= page*limit
+        // const totalProduct=productModel.countDocuments()
+
+        // productsData.skip(startIndex).limit(limit)
+
+        const allProducts= await productsData
+
+        res.status(200).json({
+            successful:true,
+            status:200,
+            data:allProducts
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+//@desc    Get a product
+//@route   GET /api/v1/product/:id
+//@access  Public
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { createProduct } from "../controllers/productControllers.js"
+import { createProduct, getAllProducts } from "../controllers/productControllers.js"
 import isLoggedin from "../middleware/isLoggedInMiddleware.js"
 
 const router= express.Router()
 
 
 router.post("/v1/product",isLoggedin,createProduct)
+router.get("/v1/products",getAllProducts)
 
 
 
